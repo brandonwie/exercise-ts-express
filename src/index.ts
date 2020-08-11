fd94748694fbc31fe7d341f7869978f00218cccb
Brandon Wie <it.brandonwie@gmail.com>
2020-08-11 19:52:03 +0800

    understanding difficulties of working with popular middleware and typescript

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+import express from 'express';
+import { router } from './routes/loginRoutes';
+
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+
+app.use('/', router);
+
+app.listen(3000, () => {
+	console.log('Server started on port 3000...');
+});
